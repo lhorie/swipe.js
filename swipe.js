bc37aedd90dd01fb03ade9db1a8cc199fbecf5d4
lhorie <Me@leo.(none)>
2012-10-13 11:33:45 -0400

    making sure not to trigger event more than once per touchend

--- swipe.js
+++ swipe.js
@@ -7,12 +7,14 @@ new function() {
 			startY = e.originalEvent.touches[0].clientY
 		},
 		touchmove: function(e) {
-			var currX = e.originalEvent.touches[0].clientX
-			var currY = e.originalEvent.touches[0].clientY
-			if (currX > startX + threshold) triggered = $(e.target).trigger("swiperight")
-			else if (currX < startX - threshold) triggered = $(e.target).trigger("swipeleft")
-			else if (currY > startY + threshold) triggered = $(e.target).trigger("swipedown")
-			else if (currY < startY - threshold) triggered = $(e.target).trigger("swipeup")
+			if (!triggered) {
+				var currX = e.originalEvent.touches[0].clientX
+				var currY = e.originalEvent.touches[0].clientY
+				if (currX > startX + threshold) triggered = $(e.target).trigger("swiperight")
+				else if (currX < startX - threshold) triggered = $(e.target).trigger("swipeleft")
+				else if (currY > startY + threshold) triggered = $(e.target).trigger("swipedown")
+				else if (currY < startY - threshold) triggered = $(e.target).trigger("swipeup")
+			}
 		},
 		touchend: function(e) {triggered = false}
 	})
