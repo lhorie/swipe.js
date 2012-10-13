ca68cf242a63e23c9fb6b0570f571cdd2c68a59b
lhorie <Me@leo.(none)>
2012-10-13 10:53:30 -0400

    adding threshold and preventing event spamming

--- swipe.js
+++ swipe.js
@@ -1,15 +1,19 @@
 new function() {
-	var startX, startY
+	var startX, startY, threshold = 50, triggered = false
 	$(document).on({
 		touchstart: function(e) {
+			triggered = false
 			startX = e.originalEvent.touches[0].clientX
 			startY = e.originalEvent.touches[0].clientY
 		},
 		touchmove: function(e) {
 			var currX = e.originalEvent.touches[0].clientX
 			var currY = e.originalEvent.touches[0].clientY
-			if (currX != startX) $(e.target).trigger("swipe" + (currX > startX ? "left" : "right"))
-			if (currY != startY) $(e.target).trigger("swipe" + (currY > startY ? "up" : "down"))
-		}
+			if (currX > startX + threshold) triggered = $(e.target).trigger("swiperight")
+			else if (currX < startX - threshold) triggered = $(e.target).trigger("swipeleft")
+			else if (currY > startY + threshold) triggered = $(e.target).trigger("swipedown")
+			else if (currY < startY - threshold) triggered = $(e.target).trigger("swipeup")
+		},
+		touchend: function(e) {triggered = false}
 	})
 }
